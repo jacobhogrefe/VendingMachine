1e9965900c04d172263f88479ec913b24cbc2c35
jacobhogrefe <jhogrefe03@gmail.com>
2023-02-14 19:49:06 -0500

    added intro and snack selection

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class VendingMachine 
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
         Snack doritos = new Snack("Doritos", 1.25);
         Snack skittles = new Snack("Skittles", 1.00);
         Snack popTarts = new Snack("Pop Tarts", 2.00);
         Snack gatorade = new Snack("Gatorade", 3.00);
-        Snack mms = new Snack("M&Ms", 1.50);
+        Snack mms = new Snack("M&Ms", 1);
+
+        System.out.println("Welcome! Here is our snack selection!");
+        System.out.println("Doritos, Skittles, Pop Tarts, Gatorade, M&Ms");
+        System.out.println("Which snack would you like to purchase?");
+        String snackSelection = input.nextLine();
+
+        if (snackSelection.equalsIgnoreCase("doritos")) {
+
+        } else if (snackSelection.equalsIgnoreCase("skittles")) {
+
+        } else if (snackSelection.equalsIgnoreCase("pop tarts")) {
+
+        } else if (snackSelection.equalsIgnoreCase("gatorade")) {
+
+        } else if (snackSelection.equalsIgnoreCase("M&Ms")) {
+            
+        } else {
+            System.out.println("Invalid Selection!");
+        }
     }
 }
